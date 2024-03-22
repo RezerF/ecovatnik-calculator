767215bf273eb037c37f9a49ae9814325b835599
Pavel Plahin <p.plahin@omp.ru>
2024-03-22 18:27:16 +0300

    update debug app

--- debug_app.py
+++ debug_app.py
@@ -14,22 +14,26 @@ st.sidebar.header('Задайте параметры расчета')
 
 
 def user_input_features():
-    sq = st.sidebar.slider('Площадь перекрытия, м2', 10, 500, 100, key='sqr_floor')
-    wi = st.sidebar.slider('Толщина перекрытия, мм', 50, 400, 0, key='width_floor')
+    sq = st.sidebar.number_input("Площадь перекрытия, м2", key='sqr_floor')
+    # sq = st.sidebar.slider('Площадь перекрытия, м2', 10, 500, 100, key='sqr_floor')
+    wi = st.sidebar.number_input('Толщина перекрытия, мм', key='width_floor')
     st.sidebar.radio('Нужна ли подготовка к утеплению?', ["Не нужна/Делаю своими силами", "Нужна"], horizontal=False,
                      key="is_floor_dop_work")
-    sq_wall = st.sidebar.slider('Площадь стен, м2', 10, 500, 100, key='sqr_wall')
-    wi_wall = st.sidebar.slider('Толщина стен, мм', 50, 400, 0, key='width_wall')
+    sq_wall = st.sidebar.number_input('Площадь стен, м2', key='sqr_wall')
+    wi_wall = st.sidebar.number_input('Толщина стен, мм', key='width_wall')
     st.sidebar.radio('Нужна ли подготовка к утеплению?', ["Не нужна/Делаю своими силами", "Нужна"],
                      key="is_wall_dop_work")
-    sq_roof = st.sidebar.slider('Площадь кровли, м2', 10, 500, 100, key='sqr_roof')
-    wi_roof = st.sidebar.slider('Толщина кровли, мм', 50, 400, 0, key='width_roof')
+    sq_roof = st.sidebar.number_input('Площадь кровли, м2', key='sqr_roof')
+    wi_roof = st.sidebar.number_input('Толщина кровли, мм', key='width_roof')
     st.sidebar.radio('Нужна ли подготовка к утеплению?', ["Не нужна/Делаю своими силами", "Нужна"],
                      key="is_roof_dop_work")
-    aaa = st.sidebar.checkbox('Show more info', value=False)
-    aaa = st.sidebar.radio('Материал фасада дома', ["Кирпич/Газобетон", "Дерево"], key="is_wood_house")
-    data = {'Площадь утепления, м2': sq,
-            'Толщина утепления, мм': wi,
+    aaa = st.sidebar.radio('Материал фасада дома', ["Кирпич/Газобетон", "Дерево"], index=1, key="is_wood_house")
+    data = {'Площадь перекрытия, м2': sq,
+            'Толщина перекрытия, мм': wi,
+            'Площадь стен, мм': sq_wall,
+            'Толщина стен, мм': wi_wall,
+            'Площадь кровли, мм': sq_roof,
+            'Толщина кровли, мм': wi_roof,
             }
     features = pd.DataFrame(data, index=[0])
     return features
@@ -38,9 +42,8 @@ def user_input_features():
 df = user_input_features()
 
 st.subheader('Расчет для параметров:')
-st.write(df)
+st.table(df)
 
-st.subheader('Результат:')
 is_dop_work = lambda dop_work: True if dop_work == "Нужна" else False
 wood_house = lambda is_wood_house: True if is_wood_house == "Дерево" else False
 
@@ -56,11 +59,25 @@ common_calc = CommonCalculator(
     is_wall_dop_work=is_dop_work(st.session_state.is_wall_dop_work),
     is_roof_dop_work=is_dop_work(st.session_state.is_roof_dop_work),
 )
-print(st.session_state)
 materials_data = common_calc.calculate_dop_work_materials()
 data_table = [TableRow(**v).get_row() for _, v in materials_data.items()]
+
+st.subheader('Материалы:')
 st.dataframe(data_table, width=1000, height=500)
+
+amount_prices = [v["amount_price"] for _, v in materials_data.items()]
+total = round(sum(amount_prices), 1)
+st.markdown(f"#### Итого материалы:  ___ {total}___ руб.   ")
+st.markdown("    ")
+st.markdown("    ")
+st.markdown("    ")
 
 works_data = common_calc.calculate_dop_work_costs()
 data_table = [TableRow(**v).get_row() for _, v in works_data.items()]
-st.dataframe(data_table)
+
+st.subheader('Работы:')
+st.dataframe(data_table, width=1000, use_container_width=True)
+
+amount_prices = [v["amount_price"] for _, v in works_data.items()]
+total = sum(amount_prices)
+st.markdown(f"#### Итого работы:  ___ {total}___ руб.")
